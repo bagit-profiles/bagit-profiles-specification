03781ae23d8b73e390a62cf0271ef78efc155069
nruest <ruestn@gmail.com>
2012-11-22 08:18:35 -0500

    some project setup stuff

---
+++ python/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+description = \
+    """
+    This module can be used to validate BagitProfiles.
+    """
+
+setup(
+      name = 'BagitProfile',
+      version = '0.0.1',
+      url = 'https://github.com/ruebot/bagit-profiles',
+      author = [
+        'Mark Jordon',
+        'Nick Ruest',
+      ],
+      author_email = [
+        'mjordan@sfu.ca',
+        'ruestn@gmail.com',
+      ],
+      py_modules = ['BagitProfile'],
+      scripts = ['BagitProfile.py'],
+      description = description,
+      platforms = ['POSIX'],
+      test_suite = 'test',
+      classifiers = [
+        'License :: Public Domain',
+        'Intended Audience :: Developers',
+        'Topic :: Communications :: File Sharing',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Filesystems',
+      ],
+)
---
+++ python/test.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+import logging
+import datetime
+import tempfile
+import unittest
+
+from os.path import join as j
+
+import BagitProfiles
