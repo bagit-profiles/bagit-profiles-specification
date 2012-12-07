841e9abdc3eeaebe0c59b5ba50fa77c2aa32e3d7
nruest <ruestn@gmail.com>
2012-12-06 20:58:12 -0500

    setup unittest

--- python/test.py
+++ python/test.py
@@ -7,4 +7,22 @@ import unittest
 
 from os.path import join as j
 
-import BagitProfiles
+import bagit_profile
+
+class Test_bag_profile(unittest.TestCase):
+
+  # Instantiate an existing Bag using https://github.com/edsu/bagit.
+  bag = bagit.Bag('test-bar')
+
+  # Instantiate a profile, supplying its URI.
+  my_profile = bagit_profile.Profile('https://raw.github.com/ruebot/bagit-profiles/refactor/cli-and-module/bagProfileBar.json')
+
+  # Validate 'Serialization' and 'Accept-Serialization'. This must be done 
+  # before .validate(bag) is called. 'mydir' is the path to the Bag.
+  
+  def test_serialization():
+    assertTrue(my_profile.validate_serialization('test-bar'))
+
+  #validate the rest of the profile/
+  def test_bag_validation():
+    assertTrue(my_profile.validate(bag))
